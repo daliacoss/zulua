da3c9d13b6ba5db5d8623768b518aee1c682f236
david <cosstropolis@gmail.com>
2015-04-22 17:19:35 -0400

    amended license

--- zulip.lua
+++ zulip.lua
@@ -2,8 +2,8 @@
 Copyright (c) Decky Coss 2014-2015 (coss@cosstropolis.com)
 
 Permission to use, copy, modify, and/or distribute this software for any purpose
-with or without fee is hereby granted, provided that the above copyright notice
-and this permission notice appear in all copies.
+with or without fee is hereby granted, provided that the above copyright notice,
+this permission notice, and the following disclaimer appear in all copies.
 
 THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
 REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
